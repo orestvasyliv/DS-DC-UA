95ea40f0c862901c297a9f10de6ea29901e24d23
OrWact <96729898+orestvasyliv@users.noreply.github.com>
2024-11-13 23:12:24 +0200

    Update pack_localisation.sh

--- decima-0.1.23/pack_localisation.sh
+++ decima-0.1.23/pack_localisation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./decima localization import --project 331139a1-8f1d-4079-ae0d-0c38f09f175d --input localization.json --output compiled
-./decima repack --backup --project 331139a1-8f1d-4079-ae0d-0c38f09f175d "/home/orwact/.local/share/Steam/steamapps/common/DEATH STRANDING DIRECTORS CUT/data/59b95a781c9170b0d13773766e27ad90.bin" compiled
+./decima repack --backup --project 331139a1-8f1d-4079-ae0d-0c38f09f175d "/.local/share/Steam/steamapps/common/DEATH STRANDING DIRECTORS CUT/data/59b95a781c9170b0d13773766e27ad90.bin" compiled
