153100507571b85d23fa63f9fa2a84a094e18e37
OrWact <vasilivorest61@gmail.com>
2025-02-17 15:48:04 +0200

    Coordinating commit with the localization changes

---
+++ decima-0.1.23/pack_localization.bat
@@ -0,0 +1,2 @@
+./decima.bat localization import --project 6fdd4975-ac12-420b-afbe-2b1fbc38f6e0 --input localization.json --output compiled
+./decima.bat repack --backup --project 6fdd4975-ac12-420b-afbe-2b1fbc38f6e0 "C:\Program Files (x86)\Steam\steamapps\common\DEATH STRANDING DIRECTORS CUT\data\59b95a781c9170b0d13773766e27ad90.bin" compiled
